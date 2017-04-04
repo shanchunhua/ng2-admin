19e0a3101bfb5087cbb3f5098a30b91fee1e7459
sam <shanchunhua@gmail.com>
2017-04-04 22:57:48 +0800

    authorize

--- src/app/pages/store/components/smartTables/custom-render.component.ts
+++ src/app/pages/store/components/smartTables/custom-render.component.ts
@@ -7,7 +7,7 @@ import { AppSettings } from '../../../../app.config.ts';
 @Component({
   template: `
   <div (click)="onSetting(value.id)" class=" btn btn-success">设置体验金比例</div>
-  <a href="{{apiEndPoint}}wechat/authorize/store/{{value.id}}" class=" btn btn-success">授权</a>
+  <a href="{{apiEndPoint}}wechat/authorize/store/{{value.id}}" target="_blank" class=" btn btn-success">授权</a>
     `
 })
 export class StoreCustomRenderComponent implements ViewCell {
