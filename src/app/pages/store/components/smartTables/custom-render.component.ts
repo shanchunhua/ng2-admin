0545e9b33c8027a02fd0f3d9061360a61a473f1e
sam <shanchunhua@gmail.com>
2017-04-04 20:54:16 +0800

    platform

--- src/app/pages/store/components/smartTables/custom-render.component.ts
+++ src/app/pages/store/components/smartTables/custom-render.component.ts
@@ -1,20 +1,21 @@
-import { Component, EventEmitter, Input, Output ,ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { ViewCell } from 'ng2-smart-table';
-import {Subscription}   from 'rxjs/Subscription';
-import {StoreService}     from './store.service';
-import {Setting} from '../setting/setting.component'
+import { Subscription } from 'rxjs/Subscription';
+import { StoreService } from './store.service';
+import { Setting } from '../setting/setting.component'
+import { AppSettings } from '../../../../app.config.ts';
 @Component({
   template: `
   <div (click)="onSetting(value.id)" class=" btn btn-success">设置体验金比例</div>
-  <a href="/#/pages/suppliers/producttable/{{value.id}}" class=" btn btn-success">产品列表</a>
+  <a href="{{apiEndPoint}}wechat/authorize/store/{{value.id}}" class=" btn btn-success">授权</a>
     `
 })
 export class StoreCustomRenderComponent implements ViewCell {
   renderValue: any;
   @Input() value: any;
-
+  apiEndPoint: string;
   constructor(private service: StoreService) {
-
+    this.apiEndPoint = AppSettings.API_ENDPOINT;
   }
   ngOnInit() {
     this.renderValue = this.value;
